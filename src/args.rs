fbcfdb37c391e40324d676501930a6d76edd1402
krishpranav <krisna.pranav@gmail.com>
2021-06-26 13:24:46 +0530

    [+] struct opt [+]

--- src/args.rs
+++ src/args.rs
@@ -3,5 +3,5 @@ use structopt::clap::AppSettings;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(global_settings = &[AppSettings::ColoredHelp], after_help=r#"The original tcpshark in C was written by spwny and released around 2001.
-tcpshark credited cyclozine for inspiration."#)]
+#[structopt(global_settings = &[AppSettings::ColoredHelp], after_help=r#"The original shijack in C was written by spwny and released around 2001.
+shijack credited cyclozine for inspiration."#)]
