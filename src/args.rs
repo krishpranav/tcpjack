c79d42d565c11a0064d938d7c5d4a7aa7bd89607
krishpranav <krisna.pranav@gmail.com>
2021-06-26 13:26:26 +0530

    [+] args [+]

--- src/args.rs
+++ src/args.rs
@@ -5,12 +5,24 @@ use structopt::StructOpt;
 #[derive(Debug, StructOpt)]
 #[structopt(global_settings = &[AppSettings::ColoredHelp], after_help=r#"The original shijack in C was written by spwny and released around 2001.
 shijack credited cyclozine for inspiration."#)]
-
 pub struct Args {
+    
     pub interface: String,
     pub src: SocketAddr,
     pub dst: SocketAddr,
 
     #[structopt(long)]
     pub seq: Option<u32>,
+
+    #[structopt(long)]
+    pub ack: Option<u32>,
+
+    #[structopt(short = "r", long)]
+    pub reset: bool,
+
+    #[structopt(short = "0", long)]
+    pub send_null: bool,
+
+    #[structopt(short, long, parse(from_occurrences))]
+    pub quiet: u8,
 }
