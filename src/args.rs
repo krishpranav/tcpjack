ba5cdff71b34b3a720615f81b4121d7091bbceb1
krishpranav <krisna.pranav@gmail.com>
2021-06-26 13:23:55 +0530

    [+] structopt [+]

--- src/args.rs
+++ src/args.rs
@@ -3,3 +3,4 @@ use structopt::clap::AppSettings;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
+#[structopt(global_settings = &[AppSettings::ColoredHelp], after_help=r#"The original shijack in C was written by spwny and released around 2001.
